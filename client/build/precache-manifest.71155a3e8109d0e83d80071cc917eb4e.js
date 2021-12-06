8a2e15323561fb1364f3dc0ba1d7c56ee0f5a767
ccsxw6 <ccsxw6@gmail.com>
2021-12-06 17:48:35 -0500

    fixing deployment

---
+++ client/build/precache-manifest.71155a3e8109d0e83d80071cc917eb4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cfcb7319f3efd70445470ab31fbc51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31b4e0165947278d840",
+    "url": "/static/css/main.61a891d0.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31b4e0165947278d840",
+    "url": "/static/js/main.5e90c560.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
